c76a7846d39a03912cbc8a95e54830daf92a2368
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 14:31:47 +0200

    Remove redundant if

--- Java/src/main/java/com/gildedrose/NormalItemWrapper.java
+++ Java/src/main/java/com/gildedrose/NormalItemWrapper.java
@@ -16,17 +16,15 @@ public class NormalItemWrapper implements ItemWrapper {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
+                }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
