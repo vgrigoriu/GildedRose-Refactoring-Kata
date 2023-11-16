b78c92b5ada8eec297e03694082ff64c587b27dc
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 14:10:28 +0200

    Instantiate LegendaryItemWrapper

--- Java/src/main/java/com/gildedrose/ItemWrapper.java
+++ Java/src/main/java/com/gildedrose/ItemWrapper.java
@@ -4,6 +4,9 @@ public interface ItemWrapper {
     void updateQuality();
 
     static ItemWrapper of(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new LegendaryItemWrapper(item);
+        }
         return new NormalItemWrapper(item);
     }
 }
