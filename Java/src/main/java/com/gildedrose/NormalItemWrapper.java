81003469f32e778e42ca557f4da92a28ed4deeb9
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 14:28:18 +0200

    Remove Aged Brie ifs

--- Java/src/main/java/com/gildedrose/NormalItemWrapper.java
+++ Java/src/main/java/com/gildedrose/NormalItemWrapper.java
@@ -8,8 +8,7 @@ public class NormalItemWrapper implements ItemWrapper {
     }
 
     public void updateQuality() {
-        if (!item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
@@ -36,18 +35,12 @@ public class NormalItemWrapper implements ItemWrapper {
         updateSellIn();
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    item.quality = 0;
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = 0;
             }
         }
     }
