7f4533f8e6acb157d147840d865508b707da456f
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 14:58:48 +0200

    Add normalizeQuality method

--- Java/src/main/java/com/gildedrose/NormalItemWrapper.java
+++ Java/src/main/java/com/gildedrose/NormalItemWrapper.java
@@ -15,6 +15,17 @@ public class NormalItemWrapper implements ItemWrapper {
         if (item.sellIn < 0) {
             updateQualityAfterSellIn();
         }
+
+        normalizeQuality();
+    }
+
+    private void normalizeQuality() {
+        if (item.quality > 50) {
+            item.quality = 50;
+        }
+        if (item.quality < 0) {
+            item.quality = 0;
+        }
     }
 
     protected void updateQualityInternal() {
