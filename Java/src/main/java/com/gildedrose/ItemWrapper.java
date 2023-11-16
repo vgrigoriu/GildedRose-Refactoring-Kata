7d4bc51843c6d1c788b3beb347e3f0f0f5b771f5
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 13:56:45 +0200

    Replace x - x with 0

--- Java/src/main/java/com/gildedrose/ItemWrapper.java
+++ Java/src/main/java/com/gildedrose/ItemWrapper.java
@@ -52,7 +52,7 @@ public class ItemWrapper {
                         }
                     }
                 } else {
-                    item.quality = item.quality - item.quality;
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
