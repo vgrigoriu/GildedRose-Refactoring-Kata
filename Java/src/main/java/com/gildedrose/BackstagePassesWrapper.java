b4536448ce964f442e0c23dc30b8dc4f73f84256
Victor Grigoriu <grigoriu@adobe.com>
2023-11-16 14:37:24 +0200

    Simplify BackstagePassesWrapper code

--- Java/src/main/java/com/gildedrose/BackstagePassesWrapper.java
+++ Java/src/main/java/com/gildedrose/BackstagePassesWrapper.java
@@ -7,24 +7,18 @@ public class BackstagePassesWrapper extends NormalItemWrapper {
 
     @Override
     public void updateQuality() {
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
 
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
+            }
 
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
@@ -32,13 +26,7 @@ public class BackstagePassesWrapper extends NormalItemWrapper {
         updateSellIn();
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                item.quality = 0;
-            }
+            item.quality = 0;
         }
     }
 }
